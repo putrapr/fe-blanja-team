86e4af23189a5fbebef57725a5537e2efbfc1c61
ChanraSB <email_addresschanrasbakkara@gmail.com>
2024-01-20 07:23:06 +0700

    button

--- src/Components/Module/Modal/ShippingAddress/ShippingAddress.jsx
+++ src/Components/Module/Modal/ShippingAddress/ShippingAddress.jsx
@@ -13,11 +13,13 @@ const ModalShippingAddress = (props) => {
       <Modal show={show} onHide={onHide} size="lg">
         <Modal.Header closeButton></Modal.Header>
         <Modal.Body>
-          <h1 className="text-center">Choose another address</h1>
-          <div className="container" style={{ width: "80%", border: "1px dashed grey", height: "100px", lineHeight: "100px", textAlign: "center", marginBottom: "40px" }}>
-            <p style={{ verticalAlign: "middle", display: "inline-block" }}>Add new address</p>
+          <h1 className="text-center" onClick={handleShowSecondModal}>
+            Choose another address
+          </h1>
+          <div className="container" onClick={handleShowSecondModal} style={{ width: "80%", border: "1px dashed grey", height: "100px", lineHeight: "100px", textAlign: "center", marginBottom: "40px" }}>
+            <p style={{ verticalAlign: "middle", display: "inline-block", cursor: "pointer" }}>Add new address</p>
           </div>
-          <div className="container" style={{ width: "80%", border: "1px solid #DB3022", boxSizing: "border-box" }}>
+          <div className="container" style={{ width: "80%", border: "1px solid #DB3022", boxSizing: "border-box", marginBottom: "100px" }}>
             <h3>Andreas Jane</h3>
             <p>Perumahan Sapphire Mediterania, Wiradadi, Kec. Sokaraja, Kabupaten Banyumas, Jawa Tengah, 53181 [Tokopedia Note: blok c 16] Sokaraja, Kab. Banyumas, 53181</p>
             <h4 style={{ color: "#DB3022", cursor: "pointer" }} onClick={handleShowSecondModal}>
