f83cf3bb74cb80617156d841922b0b33576092c7
HizkiaP <hizkiapanjaitan58@gmail.com>
2024-01-22 16:03:33 +0800

    fixed: register customer

--- src/Page/Auth/Register/customer.jsx
+++ src/Page/Auth/Register/customer.jsx
@@ -4,7 +4,7 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Image from "react-bootstrap/Image";
-import Logo from "../../../../src/assets/shopping-bag 1.svg";
+import Logo from "../../../../src/assets/icons/shopping-bag 1.svg";
 import Input from "../../../Components/Base/Input/Input";
 import "./register.css";
 import Button from "../../../Components/Base/button";
@@ -35,9 +35,13 @@ const RegisterCustomer = () => {
                   <Buttons style={{ backgroundColor: "#fff", color: "#9B9B9B", borderRadius: "0px 4px 4px 0px", width: "103px", height: "40px", fontSize: "12px", border: "1px solid #9B9B9B" }}>Seller</Buttons>
                 </ButtonGroup>
               </div>
-              <div>
+              <div className="d-flex justify-content-center">
                 <Input id="input" placeholder="Name"></Input>
+              </div>
+              <div className="d-flex justify-content-center">
                 <Input id="input" placeholder="Email"></Input>
+              </div>
+              <div className="d-flex justify-content-center">
                 <Input id="input" placeholder="Password"></Input>
               </div>
               <Button
