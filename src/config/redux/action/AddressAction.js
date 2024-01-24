b25bab0b85a172a0130f2c975e22c3d25090d8ee
ChanraSB <email_addresschanrasbakkara@gmail.com>
2024-01-25 01:26:50 +0700

    configurasi address

--- src/config/redux/action/AddressAction.js
+++ src/config/redux/action/AddressAction.js
@@ -36,3 +36,59 @@ export const addAddress = (data) => async (dispatch) => {
     });
   }
 };
+export const changeAddress = (id, data) => async (dispatch) => {
+  try {
+    dispatch({
+      type: "CHANGE_ADDRESS_REQUEST",
+    });
+    const response = await api.put(`/address/primary/${id}`, data);
+    const address = response.data.data;
+    console.log("address", response.data);
+    dispatch({
+      type: "CHANGE_ADDRESS_SUCCESS",
+      payload: address,
+    });
+  } catch (error) {
+    dispatch({
+      type: "CHANGE_ADDRESS_FAILURE",
+      payload: error.response,
+    });
+  }
+};
+export const selectedAddress = () => async (dispatch) => {
+  try {
+    dispatch({
+      type: "GET_PRIMARY_ADDRESS_REQUEST",
+    });
+    const response = await api.get(`/address/primary`);
+    const address = response.data.data;
+    console.log("Selected Address Response", response.data);
+    dispatch({
+      type: "GET_PRIMARY_ADDRESS_SUCCESS",
+      payload: address,
+    });
+  } catch (error) {
+    dispatch({
+      type: "GET_PRIMARY_ADDRESS_FAILURE",
+      payload: error.response,
+    });
+  }
+};
+export const deleteAddress = (id) => async (dispatch) => {
+  try {
+    dispatch({
+      type: "DELETE_ADDRESS_REQUEST",
+    });
+    const response = await api.delete(`/address/${id}`);
+    const address = response.data.data;
+    dispatch({
+      type: "DELETE_ADDRESS_SUCCESS",
+      payload: address,
+    });
+  } catch (error) {
+    dispatch({
+      type: "DELETE_ADDRESS_FAILURE",
+      payload: error.response,
+    });
+  }
+};
