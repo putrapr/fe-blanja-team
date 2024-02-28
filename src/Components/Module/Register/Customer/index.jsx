ccda54bc354577a2a7faebb8591d9a02a962d582
HizkiaP <hizkiapanjaitan58@gmail.com>
2024-02-28 10:53:03 +0800

    fix: bug sweet alert & add form validation

--- src/Components/Module/Register/Customer/index.jsx
+++ src/Components/Module/Register/Customer/index.jsx
@@ -33,94 +33,180 @@ const RegisterCustomer = () => {
     date_birth: "",
   });
 
-  const handleRegister = async () => {
+  const [isNameValid, setIsNameValid] = useState(true);
+  const [isEmailValid, setIsEmailValid] = useState(true);
+  const [isPasswordValid, setIsPasswordValid] = useState(true);
+
+  const handleSubmit = async (e) => {
     try {
-      const customer = await dispatch(register(values));
-      console.log(customer);
+      e.preventDefault();
+      await dispatch(register(values));
       Swal.fire({
         icon: "success",
         title: "Register Success",
         text: "Register Success",
         showConfirmButton: false,
         timer: 1000,
-      })
-      setTimeout(() => {
-        navigate("/login");
-      }, 1000);
+      });
+      navigate("/login");
     } catch (error) {
       console.log(error);
       Swal.fire({
         icon: "error",
         title: "Register Failed",
-        text: "Register Failed"
-      })
+        text: "Register Failed",
+      });
     }
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-  };
-
-  const onChange = (e) => {
+  const handleChange = (e) => {
     setValues({ ...values, [e.target.name]: e.target.value });
+
+    const ruleName = /^.{5,30}$/;
+    if (ruleName.test(values.name)) {
+      setIsNameValid(true);
+    } else {
+      setIsNameValid(false);
+    }
+
+    const ruleEmail = /^\w+([.-]?\w+)@\w+([.-]?\w+)(.\w{2,3})+$/;
+    if (ruleEmail.test(values.email)) {
+      setIsEmailValid(true);
+    } else {
+      setIsEmailValid(false);
+    }
+
+    const rulePassword = /^.{5,}$/;
+    if (rulePassword.test(values.password)) {
+      setIsPasswordValid(true);
+    } else {
+      setIsPasswordValid(false);
+    }
   };
 
   return (
     <Container id="register">
-    {tab === "registerCustomer" && 
-      <Row style={{ height: "100vh" }}>
-        <Col className="my-5">
-          <Row>
-            <Col className="mt-3">
-              <h4 className="d-flex justify-content-center logo-title">
-                <Image src={Logo} />
-                <span>Blanja</span>
-              </h4>
-            </Col>
-            <Row className="text-center">
-              <Col>
-                <p className="mt-3 text-register">Please sign up with your account</p>
-                <div>
-                  <ButtonGroup className="mt-4 mb-3">
-                    <Buttons onClick={() => setTab("registerCustomer")} style={{ backgroundColor: "#DB3022", borderRadius: "4px 0px 0px 4px", width: "103px", height: "40px", fontSize: "12px", border: "1px solid #9B9B9B" }}>Customer</Buttons>
-                    <Buttons onClick={() => setTab("registerSeller")} style={{ backgroundColor: "#fff", color: "#9B9B9B", borderRadius: "0px 4px 4px 0px", width: "103px", height: "40px", fontSize: "12px", border: "1px solid #9B9B9B" }}>Seller</Buttons>
-                  </ButtonGroup>
-                </div>
-                <form onSubmit={handleSubmit}>
-                  <div className="d-flex justify-content-center">
-                    <Input id="input" name="name" placeholder="Name" value={values.name} onChange={onChange} required={true}></Input>
-                  </div>
-                  <div className="d-flex justify-content-center">
-                    <Input id="input" name="email" placeholder="Email" value={values.email} onChange={onChange} required={true}></Input>
-                  </div>
-                  <div className="d-flex justify-content-center">
-                    <Input id="input" type="password" name="password" placeholder="Password" value={values.password} onChange={onChange} required={true}></Input>
-                  </div>
-                  <Button
-                    className="mt-5"
-                    child={loading ? "loading..." : "Register"}
-                    onClick={handleRegister}
-                    style={{
-                      backgroundColor: "#DB3022",
-                      color: "#FFFFFF",
-                      width: "400px",
-                    }}
-                  ></Button>
-                  <p className="mt-4">
-                    <Link to="/login" className="acc">
-                      Already have a account?
-                      <span style={{ color: "#DB3022" }}> Login</span>
-                    </Link>
-                  </p>
-                </form>
+      {tab === "registerCustomer" && (
+        <Row style={{ height: "100vh" }}>
+          <Col className="my-5">
+            <Row>
+              <Col className="mt-3">
+                <h4 className="d-flex justify-content-center logo-title">
+                  <Image src={Logo} />
+                  <span>Blanja</span>
+                </h4>
               </Col>
+              <Row className="text-center">
+                <Col>
+                  <p className="mt-3 text-register">
+                    Please sign up with your account
+                  </p>
+                  <div>
+                    <ButtonGroup className="mt-4 mb-3">
+                      <Buttons
+                        onClick={() => setTab("registerCustomer")}
+                        style={{
+                          backgroundColor: "#DB3022",
+                          borderRadius: "4px 0px 0px 4px",
+                          width: "103px",
+                          height: "40px",
+                          fontSize: "12px",
+                          border: "1px solid #9B9B9B",
+                        }}
+                      >
+                        Customer
+                      </Buttons>
+                      <Buttons
+                        onClick={() => setTab("registerSeller")}
+                        style={{
+                          backgroundColor: "#fff",
+                          color: "#9B9B9B",
+                          borderRadius: "0px 4px 4px 0px",
+                          width: "103px",
+                          height: "40px",
+                          fontSize: "12px",
+                          border: "1px solid #9B9B9B",
+                        }}
+                      >
+                        Seller
+                      </Buttons>
+                    </ButtonGroup>
+                  </div>
+                  <form onSubmit={handleSubmit}>
+                    <div className="d-flex justify-content-center">
+                      <Input
+                        id="input"
+                        name="name"
+                        placeholder="Full Name"
+                        value={values.name}
+                        onChange={handleChange}
+                        className={isNameValid === false ? "error-input" : ""}
+                      ></Input>
+                    </div>
+                    <p className="text-error1">
+                      {isNameValid === false
+                        ? "*Name must be at least 5 letters long"
+                        : ""}
+                    </p>
+                    <div className="d-flex justify-content-center">
+                      <Input
+                        id="input"
+                        type="email"
+                        name="email"
+                        placeholder="Email"
+                        value={values.email}
+                        onChange={handleChange}
+                        className={isEmailValid === false ? "error-input" : ""}
+                      ></Input>
+                    </div>
+                    <p className="text-error2">
+                      {isEmailValid === false
+                        ? "*You have entered an invalid email address!"
+                        : ""}
+                    </p>
+                    <div className="d-flex justify-content-center">
+                      <Input
+                        id="input"
+                        type="password"
+                        name="password"
+                        placeholder="Password"
+                        value={values.password}
+                        onChange={handleChange}
+                        className={
+                          isPasswordValid === false ? "error-input" : ""
+                        }
+                      ></Input>
+                    </div>
+                    <p className="text-error3">
+                      {isPasswordValid === false
+                        ? "*Password must be at least 5 characters long"
+                        : ""}
+                    </p>
+                    <Button
+                      type="submit"
+                      className="mt-5"
+                      child={loading ? "loading..." : "Register"}
+                      // onClick={handleRegister}
+                      style={{
+                        backgroundColor: "#DB3022",
+                        color: "#FFFFFF",
+                        width: "400px",
+                      }}
+                    ></Button>
+                    <p className="mt-4">
+                      <Link to="/login" className="acc">
+                        Already have a account?
+                        <span style={{ color: "#DB3022" }}> Login</span>
+                      </Link>
+                    </p>
+                  </form>
+                </Col>
+              </Row>
             </Row>
-            
-          </Row>
-        </Col>
-      </Row> 
-      }
-      {tab === "registerSeller" && <RegisterSeller/>}
+          </Col>
+        </Row>
+      )}
+      {tab === "registerSeller" && <RegisterSeller />}
     </Container>
   );
 };
