b8fe9c8557c0619e3822da36729cc1846e8e09ba
Putra Prasetya <putraprasetya333@gmail.com>
2024-02-29 18:16:39 +0700

    Fix Styling MyOrder Customer Profile

--- src/Components/Module/Profile/Customer/MyOrder/index.jsx
+++ src/Components/Module/Profile/Customer/MyOrder/index.jsx
@@ -1,14 +1,16 @@
 import React from "react";
+import svgNoOrder from '../../../../../assets/img/no-order.svg'
+import "./my_order.css";
 
 const MyOrder = () => {
   return (
     <section id="my-order">
       <div className="main-content">
         <div className="container">
-          <div className="wrapper-card">
+          <div className="wrapper-card bg-white">
             <h3 className="title mb-0">My Order</h3>
             <hr className="mb-3" />
-            <ul className="nav mb-3" id="pills-tab-product" role="tablist">
+            {/* <ul className="nav mb-3" id="pills-tab-product" role="tablist">
               <li className="nav-item" role="presentation">
                 <button
                   className="nav-link active"
@@ -93,7 +95,59 @@ const MyOrder = () => {
                   Order Cancel
                 </button>
               </li>
-            </ul>
+            </ul> */}
+            <table
+              className="table"
+              style={{ height: 400, border: "1px solid #D4D4D4" }}>
+              <thead>
+                <tr style={{ width: "100%", height: 60 }}>
+                  <th className="th">Tanggal</th>
+                  <th className="th">Order id</th>
+                  <th className="th">Nama barang</th>
+                  <th className="th">Jumlah</th>
+                  <th className="th">Harga</th>
+                  <th className="th">Metode Pembayaran</th>
+                </tr>
+              </thead>
+              <tbody>
+                {/* {dataOrderItems.length > 0 ? (
+                  dataOrderItems.map((item, index) => {
+                    return (
+                      <tr key={index} style={{ fontSize: 12 }}>
+                        <td style={{ padding: 10 }}>
+                          {formatDateDDMMYY(item?.order_data)}
+                        </td>
+                        <td style={{ padding: 10 }}>{item?.product_name}</td>
+                        <td style={{ padding: 10 }}>{item?.quantity_unit}</td>
+                        <td style={{ padding: 10 }}>$ {item?.price_unit}</td>
+                        <td style={{ padding: 10 }}>{item?.order_id}</td>
+                        <td style={{ padding: 10 }}>{item?.payment_method}</td>
+                      </tr>
+                    );
+                  })
+                ) : (
+                  <tr>
+                    <td colSpan="6">
+                      <div className="no-data">
+                        <img src={svgNoProduct} alt="no-order" />
+                      </div>
+                    </td>
+                  </tr>
+                )} */}
+{/* display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 340px; */}
+                <tr>
+                  <td colSpan="6">
+                    <div className="no-data">
+                      <img src={svgNoOrder} alt="no-order" />
+                    </div>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
           </div>
         </div>
       </div>
