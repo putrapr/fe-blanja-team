adfdf37cebe18b9d2af1589ddfd258654c276d81
Naim <naimabdul71@gmail.com>
2024-03-04 14:02:26 +0700

    fiks product

--- src/Components/Module/Card/index.jsx
+++ src/Components/Module/Card/index.jsx
@@ -25,47 +25,59 @@ const Card = () => {
 
   return (
     <>
-    <div>
-    {loading && <div className="d-flex align-items-center justify-content-center"><FidgetSpinner/></div>}
-    <div className="d-flex flex-wrap row-gap-4 gap-3 mb-5 mt-3 ">
-      {productList.map((product) => (
-    <div id="card" key={product.id}>
-      <div className="content " style={{ marginLeft: "6%" }}>
-        <div className="card h-100" style={{width: "200px"}}>
-          <Link to={`/product/${product.id}`} className="stretched-link"></Link>
+      <div>
+        {loading && (
+          <div className="d-flex align-items-center justify-content-center ">
+            <FidgetSpinner />
+          </div>
+        )}
+        <div className="d-flex flex-wrap row-gap-4 gap-3 mb-5 mt-3 ">
+          {productList.map((product) => (
+            <div id="card" key={product.id}>
+              <div className="content " style={{ marginLeft: "6%" }}>
+                <div className="card h-100" style={{ width: "200px" }}>
+                  <Link
+                    to={`/product/${product.id}`}
+                    className="stretched-link"
+                  ></Link>
 
-          <img
-            crossOrigin="anonymous"
-            src={product.image}
-            className="card-img-top"
-            alt="cardImage"
-            style={{ height: "136px" }}
-          />
-          <div className="card-body d-flex flex-column justify-content-between">
-            <h4 className="card-title text-dark" style={{ fontSize: "16px" }}>
-            {product.name}
-            </h4>
-            <div>
-              <h5 className="text-danger metropolis-b">Rp. {product.price}</h5>
-              <span className="card-text text-secondary text-break">
-                Zalora Cloth
-              </span>
-              <div>
-                <img src={iconStar} alt="Star" />
-                <img src={iconStar} alt="Star" />
-                <img src={iconStar} alt="Star" />
-                <img src={iconStar} alt="Star" />
-                <img src={iconStar} alt="Star" />
-                <span className="text-secondary">(5)</span>
+                  <img
+                    crossOrigin="anonymous"
+                    src={product.image}
+                    className="card-img-top"
+                    alt="cardImage"
+                    style={{ height: "136px" }}
+                  />
+                  <div className="card-body d-flex flex-column justify-content-between">
+                    <h4
+                      className="card-title text-dark"
+                      style={{ fontSize: "16px" }}
+                    >
+                      {product.name}
+                    </h4>
+                    <div>
+                      <h5 className="text-danger metropolis-b">
+                        Rp. {product.price}
+                      </h5>
+                      <span className="card-text text-secondary text-break">
+                        Zalora Cloth
+                      </span>
+                      <div>
+                        <img src={iconStar} alt="Star" />
+                        <img src={iconStar} alt="Star" />
+                        <img src={iconStar} alt="Star" />
+                        <img src={iconStar} alt="Star" />
+                        <img src={iconStar} alt="Star" />
+                        <span className="text-secondary">(5)</span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
+          ))}
         </div>
       </div>
-    </div>
-    ))}
-    </div>
-    </div>
     </>
   );
 };
